b3dda73d062dcb80736e2f1397ccca0e7f1405be
Yara Diaz de Cerio <yaracerio99@outlook.es>
2021-02-19 17:20:15 +0100

    cambio pass sin terminar

--- Lab 2 y 3/Class1.vb
+++ Lab 2 y 3/Class1.vb
@@ -81,6 +81,56 @@ Public Class accesoDatosSQL
         Return True
     End Function
 
+    Public Shared Function modificarContraseña(ByVal email As String) As Integer
+        Try
+            'Direccion de origen
+            Dim from_address As New MailAddress("emailconfirmation14@gmail.com", "Yara Diaz de Cerio")
+            'Direccion de destino
+            Dim to_address As New MailAddress(email)
+            'Password de la cuenta
+            Dim from_pass As String = "pepe14pruebas"
+            'Objeto para el cliente smtp
+            Dim smtp As New SmtpClient
+            'Host en este caso el servidor de gmail
+            smtp.Host = "smtp.gmail.com"
+            'Puerto
+            smtp.Port = 587
+            'SSL activado para que se manden correos de manera segura
+            smtp.EnableSsl = True
+            'No usar los credenciales por defecto ya que si no no funciona
+            smtp.UseDefaultCredentials = False
+            'Credenciales
+            smtp.Credentials = New System.Net.NetworkCredential(from_address.Address, from_pass)
+            'Creamos el mensaje con los parametros de origen y destino
+            Dim message As New MailMessage(from_address, to_address)
+            'Añadimos el asunto
+            message.Subject = "subject"
+
+            Dim codpass As Integer
+            Randomize()
+            codpass = Int((100000 * Rnd()) + 1)
+
+            Dim st = "update Usuarios set codpass=" + codpass + "where email=" + email + ";"
+            Dim numregs As Integer
+            comando = New SqlCommand(st, conexion)
+            Try
+                numregs = comando.ExecuteNonQuery()
+            Catch ex As Exception
+                Return ex.Message
+            End Try
+            'Concatenamos el cuerpo del mensaje a la plantilla
+            message.Body = "<html><head></head><body>" + "confirmar registro" + "</body></html>"
+            'Definimos el cuerpo como html para poder escojer mejor como lo mandamos
+            message.IsBodyHtml = True
+            'Se envia el correo
+            smtp.Send(message)
+        Catch e As Exception
+            Return False
+        End Try
+        Return True
+    End Function
+
+
 
 
 End Class
