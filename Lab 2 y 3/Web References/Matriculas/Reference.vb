42ec7fc4c2129ad297ee4474c5390f7823d3f4c8
yakodelaragua <araycdd@gmail.com>
2021-04-20 22:02:28 +0200

    Corregido

--- Lab 2 y 3/Web References/Matriculas/Reference.vb
+++ Lab 2 y 3/Web References/Matriculas/Reference.vb
@@ -22,70 +22,70 @@ Imports System.Xml.Serialization
 'Microsoft.VSDesigner generó automáticamente este código fuente, versión=4.0.30319.42000.
 '
 Namespace Matriculas
-
+    
     '''<remarks/>
-    <System.CodeDom.Compiler.GeneratedCodeAttribute("System.Web.Services", "4.8.3752.0"),
-     System.Diagnostics.DebuggerStepThroughAttribute(),
-     System.ComponentModel.DesignerCategoryAttribute("code"),
-     System.Web.Services.WebServiceBindingAttribute(Name:="MatriculasBinding", [Namespace]:="https://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl")>
+    <System.CodeDom.Compiler.GeneratedCodeAttribute("System.Web.Services", "4.8.4084.0"),  _
+     System.Diagnostics.DebuggerStepThroughAttribute(),  _
+     System.ComponentModel.DesignerCategoryAttribute("code"),  _
+     System.Web.Services.WebServiceBindingAttribute(Name:="MatriculasBinding", [Namespace]:="https://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl")>  _
     Partial Public Class Matriculas
         Inherits System.Web.Services.Protocols.SoapHttpClientProtocol
-
+        
         Private comprobarOperationCompleted As System.Threading.SendOrPostCallback
-
+        
         Private useDefaultCredentialsSetExplicitly As Boolean
-
+        
         '''<remarks/>
         Public Sub New()
             MyBase.New
             Me.Url = Global.Lab_2_y_3.My.MySettings.Default.Lab_2_y_3_Matriculas_Matriculas
-            If (Me.IsLocalFileSystemWebService(Me.Url) = True) Then
-                Me.UseDefaultCredentials = True
-                Me.useDefaultCredentialsSetExplicitly = False
+            If (Me.IsLocalFileSystemWebService(Me.Url) = true) Then
+                Me.UseDefaultCredentials = true
+                Me.useDefaultCredentialsSetExplicitly = false
             Else
-                Me.useDefaultCredentialsSetExplicitly = True
+                Me.useDefaultCredentialsSetExplicitly = true
             End If
         End Sub
-
+        
         Public Shadows Property Url() As String
             Get
                 Return MyBase.Url
             End Get
             Set
-                If (((Me.IsLocalFileSystemWebService(MyBase.Url) = True) _
-                            AndAlso (Me.useDefaultCredentialsSetExplicitly = False)) _
-                            AndAlso (Me.IsLocalFileSystemWebService(Value) = False)) Then
-                    MyBase.UseDefaultCredentials = False
+                If (((Me.IsLocalFileSystemWebService(MyBase.Url) = true)  _
+                            AndAlso (Me.useDefaultCredentialsSetExplicitly = false))  _
+                            AndAlso (Me.IsLocalFileSystemWebService(value) = false)) Then
+                    MyBase.UseDefaultCredentials = false
                 End If
-                MyBase.Url = Value
+                MyBase.Url = value
             End Set
         End Property
-
+        
         Public Shadows Property UseDefaultCredentials() As Boolean
             Get
                 Return MyBase.UseDefaultCredentials
             End Get
             Set
-                MyBase.UseDefaultCredentials = Value
-                Me.useDefaultCredentialsSetExplicitly = True
+                MyBase.UseDefaultCredentials = value
+                Me.useDefaultCredentialsSetExplicitly = true
             End Set
         End Property
-
+        
         '''<remarks/>
         Public Event comprobarCompleted As comprobarCompletedEventHandler
-
+        
         '''<remarks/>
-        <System.Web.Services.Protocols.SoapRpcMethodAttribute("http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php/comprobar", RequestNamespace:="https://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl", ResponseNamespace:="https://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl")>
+        <System.Web.Services.Protocols.SoapRpcMethodAttribute("http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php/comprobar", RequestNamespace:="https://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl", ResponseNamespace:="https://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl")>  _
         Public Function comprobar(ByVal x As String) As <System.Xml.Serialization.SoapElementAttribute("z")> String
             Dim results() As Object = Me.Invoke("comprobar", New Object() {x})
-            Return CType(results(0), String)
+            Return CType(results(0),String)
         End Function
-
+        
         '''<remarks/>
         Public Overloads Sub comprobarAsync(ByVal x As String)
             Me.comprobarAsync(x, Nothing)
         End Sub
-
+        
         '''<remarks/>
         Public Overloads Sub comprobarAsync(ByVal x As String, ByVal userState As Object)
             If (Me.comprobarOperationCompleted Is Nothing) Then
@@ -93,56 +93,56 @@ Namespace Matriculas
             End If
             Me.InvokeAsync("comprobar", New Object() {x}, Me.comprobarOperationCompleted, userState)
         End Sub
-
+        
         Private Sub OncomprobarOperationCompleted(ByVal arg As Object)
             If (Not (Me.comprobarCompletedEvent) Is Nothing) Then
-                Dim invokeArgs As System.Web.Services.Protocols.InvokeCompletedEventArgs = CType(arg, System.Web.Services.Protocols.InvokeCompletedEventArgs)
+                Dim invokeArgs As System.Web.Services.Protocols.InvokeCompletedEventArgs = CType(arg,System.Web.Services.Protocols.InvokeCompletedEventArgs)
                 RaiseEvent comprobarCompleted(Me, New comprobarCompletedEventArgs(invokeArgs.Results, invokeArgs.Error, invokeArgs.Cancelled, invokeArgs.UserState))
             End If
         End Sub
-
+        
         '''<remarks/>
         Public Shadows Sub CancelAsync(ByVal userState As Object)
             MyBase.CancelAsync(userState)
         End Sub
-
+        
         Private Function IsLocalFileSystemWebService(ByVal url As String) As Boolean
-            If ((url Is Nothing) _
+            If ((url Is Nothing)  _
                         OrElse (url Is String.Empty)) Then
-                Return False
+                Return false
             End If
             Dim wsUri As System.Uri = New System.Uri(url)
-            If ((wsUri.Port >= 1024) _
+            If ((wsUri.Port >= 1024)  _
                         AndAlso (String.Compare(wsUri.Host, "localHost", System.StringComparison.OrdinalIgnoreCase) = 0)) Then
-                Return True
+                Return true
             End If
-            Return False
+            Return false
         End Function
     End Class
-
+    
     '''<remarks/>
-    <System.CodeDom.Compiler.GeneratedCodeAttribute("System.Web.Services", "4.8.3752.0")>
+    <System.CodeDom.Compiler.GeneratedCodeAttribute("System.Web.Services", "4.8.4084.0")>  _
     Public Delegate Sub comprobarCompletedEventHandler(ByVal sender As Object, ByVal e As comprobarCompletedEventArgs)
-
+    
     '''<remarks/>
-    <System.CodeDom.Compiler.GeneratedCodeAttribute("System.Web.Services", "4.8.3752.0"),
-     System.Diagnostics.DebuggerStepThroughAttribute(),
-     System.ComponentModel.DesignerCategoryAttribute("code")>
+    <System.CodeDom.Compiler.GeneratedCodeAttribute("System.Web.Services", "4.8.4084.0"),  _
+     System.Diagnostics.DebuggerStepThroughAttribute(),  _
+     System.ComponentModel.DesignerCategoryAttribute("code")>  _
     Partial Public Class comprobarCompletedEventArgs
         Inherits System.ComponentModel.AsyncCompletedEventArgs
-
+        
         Private results() As Object
-
+        
         Friend Sub New(ByVal results() As Object, ByVal exception As System.Exception, ByVal cancelled As Boolean, ByVal userState As Object)
             MyBase.New(exception, cancelled, userState)
             Me.results = results
         End Sub
-
+        
         '''<remarks/>
         Public ReadOnly Property Result() As String
             Get
-                Me.RaiseExceptionIfNecessary()
-                Return CType(Me.results(0), String)
+                Me.RaiseExceptionIfNecessary
+                Return CType(Me.results(0),String)
             End Get
         End Property
     End Class
